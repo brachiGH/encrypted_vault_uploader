b0b2336207dbe7c413bf994281d58629c03e5229
brachi <ebsd_kdeutils@aleeas.com>
2023-12-16 08:44:12 +0100

    fix err went last sync file does not exist

--- libs/synclib.py
+++ libs/synclib.py
@@ -6,12 +6,17 @@ def register_a_sync(datetime = datetime.now().strftime("%d-%m-%Y__%H-%M-%S")):
     f.write(datetime)
     f.close()
 
-def get_last_register():
-    f=open('date.lastsync', 'r')
-    time = f.readline()
-    f.close()
-
-    return time.strip()
+def get_last_register(default_value='16-12-2023__01-00-00'):
+    try:
+        with open('date.lastsync', 'r') as f:
+            time = f.readline()
+            return time.strip()
+    except FileNotFoundError:
+        print("File 'date.lastsync' not found. Returning default value.")
+        return default_value
+    except Exception as e:
+        print(f"Error reading file: {e}")
+        return default_value
 
 
 def filter_elements_by_date(elements, target_date_time):
